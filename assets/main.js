41edc40c5c1a91f965c69850f59572c06ea667a9
moatru <moasteve@gmail.com>
2020-02-21 15:20:57 +0800

    edits

--- assets/main.js
+++ assets/main.js
@@ -12,24 +12,6 @@ $(document).ready(function() {
 	// end hamburger menu
 console.log("bob")
 	// This is the pop-up gallery
-	$('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title');
-			}
-		}
-	});
-
 	var allPanels = $('.accordion > dd').hide();
 
 		$('.accordion > dt > a').click(function() {
